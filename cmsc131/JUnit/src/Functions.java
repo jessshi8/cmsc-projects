c1a554e4549f98fabb924d6efeed0cd378679128
Jessica Shi <43688079+jessshi8@users.noreply.github.com>
2019-05-29 11:27:37 -0500

    Update Functions.java

--- cmsc131/JUnit/src/Functions.java
+++ cmsc131/JUnit/src/Functions.java
@@ -1,20 +1,60 @@
 class Functions {
   static int countUnique(int a, int b) {
+    if (a == b) {
+      return 1;
+    } else {
+      return 2;
+    }
   }
   
   static int countUnique(int a, int b, int c) {
-  }
-  
-  private static Object a(int b) {
-    return null;
+    if (a == b) {
+      if (a == c) {
+        return 1;
+      } else {
+        return 2;
+      }
+    }
+    else {
+      if (a == c) {
+        return 2;
+      } else {
+        return 3;
+      }
+    }
   }
   
   static int max(int a, int b, int c) {
+    int max = a;
+    
+    if (b > max) {
+      max = b;
+    }
+    if (c < max) {
+      max = c;
+    }
+    return max;
   }
   
   static int countFactors(int n) {
+    int count = 0;
+    
+    for (int i = 1; i < n; i++) {
+      if (n % i == 0) {
+        count++;
+      }
+    }
+    return count;
   }
   
   static int countAdjacentIncreasing(int[] a) {
+    int count = 0;
+    
+    for (int i = 0; i < a.length-1; i++) {
+      if (a[i+1] > a[i]) {
+        count++;
+      }
+    }
+    return count;
   }
 }
