bc7338c5b03f1a6dbe418e52a6980def456b2bda
Jessica Shi <43688079+jessshi8@users.noreply.github.com>
2019-05-29 11:18:18 -0500

    Create Functions.java

---
+++ cmsc131/JUnit/src/Functions.java
@@ -0,0 +1,20 @@
+class Functions {
+  static int countUnique(int a, int b) {
+  }
+  
+  static int countUnique(int a, int b, int c) {
+  }
+  
+  private static Object a(int b) {
+    return null;
+  }
+  
+  static int max(int a, int b, int c) {
+  }
+  
+  static int countFactors(int n) {
+  }
+  
+  static int countAdjacentIncreasing(int[] a) {
+  }
+}
